aaaca274f0227c385bf8e1eefd0ff63755344822
damoosnz <damien.legrand@bimsolutionscentre.com>
2024-09-23 15:42:33 +1200

    add html injection

--- script/plannings/loader-planning.js
+++ script/plannings/loader-planning.js
@@ -1,3 +1,23 @@
 import jQuery from "https://cdn.skypack.dev/jquery";
+
+// inject html
+async function loadHTML(filePath) {
+    const response = await fetch(filePath);
+    const html = await response.text();
+    document.body.innerHTML = html; // Replace the entire body content
+}
+
+// Load your modular HTML files
+async function loadModularContent() {
+    await loadHTML('html\global\header.html'); // Use relative path
+    await loadHTML('html\plannings\combine-sheets.html'); // Use relative path
+    await loadHTML('html\global\footer.html'); // Use relative path
+}
+
+// Call the function when the page loads
+window.onload = loadModularContent;
+
+
+// inject scripts
 import './combine-sheets.js'
 
