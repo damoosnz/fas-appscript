1cb8202ae82621047a1571e44504ee4d9236a4f9
damoosnz <damien.legrand@bimsolutionscentre.com>
2024-09-26 17:36:15 +1200

    add  init planning

--- script/general/load-html.js
+++ script/general/load-html.js
@@ -1,7 +1,13 @@
 // Inject HTML
-export async function loadHTML(filePath) {
+export async function loadHTML(filePath, id) {
     const response = await fetch(filePath);
     const html = await response.text();
     console.log(filePath, html)
+
+    if(id) {
+        html.replace('{{id}}', id)
+
+    }
+
     document.body.innerHTML += html; // Append the HTML
 }
---
+++ script/plannings/loaders/loader-init-planning.js
@@ -0,0 +1,37 @@
+import { loadScripts } from "../../general/load-scripts.js";
+import { loadHTML } from "../../general/load-html.js";
+
+// log the start of the script
+
+console.log('page init started')
+
+const githubPagesUrl = 'https://damoosnz.github.io/fas-appscript/'
+async function loadModularContent() {
+
+    // Load your modular HTML files
+    console.log('html loading started')
+    await loadHTML(githubPagesUrl + 'html/global/header.html'); 
+    await loadHTML(githubPagesUrl + 'html/global/title.html'); 
+    await loadHTML(githubPagesUrl + 'html/global/button.html');
+    await loadHTML(githubPagesUrl + 'html/global/title.html'); 
+    await loadHTML(githubPagesUrl + 'html/global/button.html');
+    await loadHTML(githubPagesUrl + 'html/global/title.html'); 
+    await loadHTML(githubPagesUrl + 'html/global/button.html');
+    await loadHTML(githubPagesUrl + 'html/global/footer.html'); 
+    console.log('html loading completed')
+    
+    // Load scripts after HTML content is added
+    console.log('script loading started')
+    loadScripts('script/plannings/init-planning');
+    console.log('script loading completed')
+
+    // add event listeners
+
+
+    console.log('page init completed')
+}
+
+
+
+// Call the function when the DOM is fully loaded
+document.addEventListener('DOMContentLoaded', loadModularContent);
