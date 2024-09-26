812e31b4d22f209ec6854cfad518d6debae64c73
damoosnz <damien.legrand@bimsolutionscentre.com>
2024-09-26 22:41:49 +1200

    debug

--- script/plannings/init-planning.js
+++ script/plannings/init-planning.js
@@ -4,11 +4,13 @@ function initPlanning() {
 
     $('#button-1').text("Combine Sheets")
     $('#button-2').text("Extract Data")
-    $('#button-3').text("Send to Fas")
+    $('#button-3').text("Send to Fas Post")
+    $('#button-4').text("Send to Knack")
 
     $('#title-1').text("Step 1")
     $('#title-2').text("Step 2")
     $('#title-3').text("Step 3")
+    $('#title-4').text("Step 4")
     
     $('#button-1').on('click', function (event) {
         event.preventDefault(); // Prevent navigation
@@ -35,6 +37,15 @@ function initPlanning() {
             // Close the dialog
         })
         .sendToFasPostPlanning();        
+    });
+
+    $('#button-4').on('click', function (event) {
+        event.preventDefault(); // Prevent navigation
+        google.script.run
+        .withSuccessHandler(function () {
+            // Close the dialog
+        })
+        .sendToFasPostPlanning(); // to update to fas post to knack     
     });
 
 }
