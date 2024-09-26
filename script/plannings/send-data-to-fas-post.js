20e10bc6b82cb239838790f68a957d559a13ee89
damoosnz <damien.legrand@bimsolutionscentre.com>
2024-09-26 22:38:37 +1200

    debug

--- script/plannings/send-data-to-fas-post.js
+++ script/plannings/send-data-to-fas-post.js
@@ -8,11 +8,11 @@ function sendDataToFasPost() {
 
     google.script.run
         .withSuccessHandler(function () {
-            $('#linkButton').text("Go to FAS Post Planning")
+            $('#button-1').text("Go to FAS Post Planning")
             hideDivById('progress')
             showDivById('button-1')
             // Close the dialog
-            $('#linkButton').on('click', function () {
+            $('#button-1').on('click', function () {
                 google.script.host.close();  // This will close the dialog
                 window.open('https://docs.google.com/spreadsheets/d/1MTd4HeY2zX86c-QH6jhs8_hoNnw7pMT-1_h1CDpEplQ/edit?gid=0#gid=0', '_blank')
             });
