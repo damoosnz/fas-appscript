cb604f91bb0d2b5f0f9bac96ef0ba62729faaa46
damoosnz <damien.legrand@bimsolutionscentre.com>
2024-09-26 21:41:28 +1200

    debug

--- script/plannings/loaders/loader-init-planning.js
+++ script/plannings/loaders/loader-init-planning.js
@@ -1,5 +1,5 @@
 import { loadScripts } from "../../general/load-scripts.js";
-import { loadHTML } from "../../general/load-html.mjs";
+import { loadHTML } from "../../general/load-html.js";
 
 // log the start of the script
 
--- script/plannings/loaders/loader-send-data-to-fas-post.js
+++ script/plannings/loaders/loader-send-data-to-fas-post.js
@@ -1,5 +1,5 @@
 import { loadScripts } from "../../general/load-scripts.js";
-import { loadHTML } from "../../general/load-html.mjs";
+import { loadHTML } from "../../general/load-html.js";
 import { showDivById, hideDivById } from "../../general/show-progess.js";
 
 // log the start of the script
