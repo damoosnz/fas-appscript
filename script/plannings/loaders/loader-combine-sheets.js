bc8907acd8fcb8ed41fee5b6e4e4956906292487
damoosnz <damien.legrand@bimsolutionscentre.com>
2025-12-05 08:18:54 +1300

    updatd combine sheets

--- script/plannings/loaders/loader-combine-sheets.js
+++ script/plannings/loaders/loader-combine-sheets.js
@@ -21,7 +21,7 @@ async function loadModularContent() {
 
     // CONTENT
     const contentRef = 'html\modal\combine-sheets.html'
-    const $content = await $html.$content(contentRef)
+    const $content = await $html.$modal(contentRef)
     $cont.append($header)
 
     const $list = $content.find('#sheetsList')
